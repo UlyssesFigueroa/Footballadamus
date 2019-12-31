7a9a220c30686038795107690418d704e3e16653
Ulysses <This2willpass@aol.com>
2019-12-30 23:46:50 -0700

    React Updates

---
+++ build/precache-manifest.620fe76d9c12a7d838c9f9ce04b003ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c972f52c5e82038e64243367e3adf9e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82abbc27dc98c9895b77",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "4d27519d440587539d60",
+    "url": "/static/js/2.d520b7b6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.d520b7b6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "82abbc27dc98c9895b77",
+    "url": "/static/js/main.8ab886f1.chunk.js"
+  },
+  {
+    "revision": "cf08e45bb1d20433e6ac",
+    "url": "/static/js/runtime-main.14f518ab.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
