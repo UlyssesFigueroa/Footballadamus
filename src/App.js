93eb7d77cbf4d02e2cc0d7ce314d5b68246d8eeb
Jorge Lopez <JorgeLopez5252@gmail.com>
2020-01-02 10:52:23 -0700

    updated with images folder

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
+import Navbar from "./components/Navbar";
+// import TableArea from "./components/TableArea";
 import Jumbotron from "./components/Jumbotron";
-
 
 function App() {
   return (
     <div className="container">
+      <Navbar/>
       <Jumbotron />
+      
     </div>
   );
 }
