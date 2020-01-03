fa93d88ed30f08f34b4a5da45b970dc9ab1589de
Jorge Lopez <JorgeLopez5252@gmail.com>
2020-01-03 14:42:53 -0700

    updated new compo, will be adding the routed from compo to compo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import SignUp from "./components/SignUp";
+// import Selection from "./components/Selection";
+// import PlayerChoices from "./components/Playerchoices";
 // import TableHeadings from "./components/TableHeadings";
 // import TableData from "./components/TableData";
 // import PlayerQue from "./components/PlayerQue";
 // import OpenGames from "./components/OpenGames";
+
+const choices = ["tackle", "fumble", "reception", "field goal"]
 
 function App() {
   return (
@@ -12,6 +16,7 @@ function App() {
       <Navbar/>
       {/* <OpenGames/> */}
       {/* <PlayerQue /> */}
+      {/* <PlayerChoices choices={choices}/> */}
       {/* <TableHeadings/> */}
       {/* <TableData/> */}
       <SignUp />
--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,7 +1,43 @@
 import React from "react";
 
+const divStyle = {
+    fontSize: '30px',
+    textAlign: 'center'
+
+};
+
+
+const choices = ["tackle", "fumble", "reception", "field goal"]
+// ReactDOM.render(
+//     <PlayerChoices choices={choices}/>,
+//     document.getElementById('listitem')
+// );
+
+
 function Selection() {
-    return(
-        
+    return (
+        <div className="container">
+
+            <h1 style={divStyle}>Room 1</h1>
+            <h2 style={divStyle}>WAS vs PHI</h2>
+
+            <article class="message">
+                <div class="message-header">
+                    <p>Live Updates</p>
+                    <button class="delete" aria-label="delete"></button>
+                </div>
+                <div class="message-body">
+                    "Case Keenum passed to Vernon Davis to the right for 48 yard touchdown"
+  </div>
+            </article>
+
+
+
+            <div id="listitem"></div>
+
+        </div>
+
     )
 }
+
+export default Selection;
