7c9564b65817645006f3219c96fb53b966bcf500
Jorge Lopez <JorgeLopez5252@gmail.com>
2020-01-06 23:35:13 -0700

    updates and added a sign in page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 // import {Text, View} from 'react-native';
 import Navbar from "./components/Navbar";
 import SignUp from "./components/SignUp";
+import SignIn from "./components/SignIn";
 import Selection from "./components/Selection";
 import PlayerChoices from "./components/Playerchoices";
 // import TableHeadings from "./components/TableHeadings";
@@ -25,6 +26,7 @@ function App() {
       <Route exact path="/profile" component={Profile}/>
       <Route exact path="/playerque" component={PlayerQue}/>
       <Route exact path="/signup" component={SignUp}/>
+      <Route exact path="/signin" component={SignIn}/>
       <Route exact path="/selection" component={Selection}/>
       <PlayerChoices choices={choices}/>
       {/* <TableHeadings/> */}
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,6 +1,5 @@
 import React from "react";
-import {Link} from "react-router-dom";
-
+import { Link } from "react-router-dom";
 
 // will link styles later
 const headerStyle = {
@@ -41,10 +40,33 @@ function Welcome() {
         <br />
         <h2>Ready to begin?</h2>
 
-        <button className="button"><Link to="/signup" className={window.location.pathname === "/signup" ? "nav-link active" : "nav-link"}>
-                    Get Started
-        </Link></button>
+        <div style={ruleStyle} className="buttons">
+          <button className="button">
+            <Link
+              to="/signup"
+              className={
+                window.location.pathname === "/signup"
+                  ? "nav-link active"
+                  : "nav-link"
+              }
+            >
+              Create an account
+            </Link>
+          </button>
 
+          <button className="button">
+            <Link
+              to="/signin"
+              className={
+                window.location.pathname === "/signin"
+                  ? "nav-link active"
+                  : "nav-link"
+              }
+            >
+              Sign back in
+            </Link>
+          </button>
+        </div>
       </div>
     </div>
   );
