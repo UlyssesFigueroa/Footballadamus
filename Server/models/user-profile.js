08159834f6d5a07235e7925fbbf6115d3d396798
Jorge Lopez <JorgeLopez5252@gmail.com>
2020-01-09 10:38:57 -0700

    removed server file

--- Server/models/user-profile.js
+++
@@ -1,9 +0,0 @@
-var UserProfileModel = function(cnf) {
-    this.email = cnf.email,
-    this.userName = cnf.userName,
-    this.firstName = cnf.firstName,
-    this.lastName = cnf.lastName,
-    this.city = cnf.city,
-    this.state = cnf.state
-};
-module.exports = UserProfileModel;
